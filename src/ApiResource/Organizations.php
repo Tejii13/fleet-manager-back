9c8708e68b181df7941f30af80c780661438c60c
Tejii13 <telmosousapro@proton.me>
2023-11-08 09:22:17 +0100

    changed delete to post (temp fix)

--- src/ApiResource/Organizations.php
+++ src/ApiResource/Organizations.php
@@ -5,7 +5,7 @@ namespace App\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
 use App\State\OrganizationsShipsProvider;
 use App\State\OrganizationsUsersProvider;
 use App\State\OrganizationUserRemovingProcessor;
@@ -13,7 +13,7 @@ use App\State\OrganizationUserRemovingProcessor;
 #[ApiResource(operations: [
   new Get(uriTemplate: '/organizations/{id}/ships', provider: OrganizationsShipsProvider::class),
   new Get(uriTemplate: '/organizations/{id}/users', provider: OrganizationsUsersProvider::class),
-  new Delete(uriTemplate: '/organizations/{id}/users/{userId}', processor: OrganizationUserRemovingProcessor::class),
+  new Post(uriTemplate: '/organizations/{id}/users/{userId}', processor: OrganizationUserRemovingProcessor::class),
 ])]
 class Organizations
 {
